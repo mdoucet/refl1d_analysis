aa983928df7aacb5e001d90db766a849d6b037f5
Mathieu Doucet <doucetm@ornl.gov>
2017-09-27 14:21:49 -0400

    add magnetism output

--- src/refl1d_model.py
+++ src/refl1d_model.py
@@ -1,7 +1,19 @@
 """
+    Process chains from a refl1d output population and extract statistics
 """
 import logging
 import json, re
+import time
+import argparse
+import os
+import numpy as np
+from collections import OrderedDict
+from refl1d.names import *
+from refl1d.probe import make_probe
+from refl1d.rebin import rebin
+from bumps import dream
+from refl1d.errors import calc_errors
+from bumps.cli import load_model
 
 def parse_single_param(line):
     """
@@ -42,6 +54,37 @@ def parse_single_param(line):
     return par_name, value_float, error_float
 
 
+class Layer(object):
+    """ Layer representation """
+    def __init__(self, name, parameters):
+        self.name = name
+        self.is_magnetic = False
+        self.rho = 0
+        self.irho = 0
+        self.thickness = 0
+        self.interface = 0
+        
+        for p, value in parameters.items():
+            par_name = p.replace(name, '').strip()
+            if p.endswith('M') or 'interfaceM' in p or 'deadM' in p:
+                self.is_magnetic = True
+                setattr(self, par_name.replace('M ', '_'), value)
+            else:
+                setattr(self, par_name, value)
+
+    def material(self):
+        return "%s = SLD(name='%s', rho=%s, irho=%s)" % (self.name, self.name, self.rho, self.irho)
+
+    def layer(self):
+        if self.is_magnetic:
+            mag = "rhoM=%s, thetaM=%s, interface_above=%s, interface_below=%s, dead_above=%s, dead_below=%s" % \
+            (self.rhoM, self.thetaM, self.interface_above, self.interface_below, self.dead_above, self.dead_below)
+            return "%s(%s, %s, magnetism=Magnetism(%s))" % (self.name, self.thickness, self.interface, mag)
+        return "%s(%s, %s)" % (self.name, self.thickness, self.interface)
+
+    def __repr__(self):
+        return "%s\n%s" % (self.material(), self.layer())
+        
 class ReflectivityModel(object):
     def __init__(self, info, layers):
         self.refl_model = info
@@ -56,6 +99,18 @@ class ReflectivityModel(object):
             self.chi2 = info['chi2']
         else:
             self.chi2 = 0
+
+    def convert_to_refl1d(self):
+        """ Convert this model to a refl1d model """
+        materials = ''
+        slabs = []
+        for name, layer in self.layers.items():
+            layer_obj = Layer(name, layer)
+            materials += "%s\n" % layer_obj.material()
+            slabs.append(layer_obj.layer())
+        sample = "sample = (%s)" % '\n    | '.join(slabs)
+        exec "%s\n%s" % (materials, sample)
+        return sample
 
     def __repr__(self):
         """ Pretty print this model """
@@ -119,7 +174,7 @@ class ReflectivityProblem(object):
         refl_model = dict(data_path="none")
         current_layer = {}
         current_layer_name = None
-        layers = {}
+        layers = OrderedDict()
         chi2 = 0
 
         in_probe = False
@@ -152,7 +207,7 @@ class ReflectivityProblem(object):
             if l.startswith("-- Model"):
                 result = re.search(r"-- Model (\d+)", l)
                 if result is not None:
-                    layers = {}
+                    layers = OrderedDict()
                     current_layer = {}
                     current_layer_name = None
                     if len(model_names) <= int(result.group(1)):
@@ -242,6 +297,60 @@ class ReflectivityProblem(object):
                 layer_name = toks[0]
                 for m in self.model_list.keys():
                     self.model_list[m].layers[layer_name][par_name] = parameter_list[i]
+
+    def convert_to_model(self):
+        """ Convert this model into a refl1d model """
+        sld_profiles = []
+        for name, m in self.model_list.items():
+            sample = m.convert_to_refl1d()
+            ones = np.arange(0.01, 0.1, 0.01)
+            pp = make_probe(T=ones, dT=ones, L=ones, dL=ones, data=(ones, ones), radiation = 'neutron')
+            probe = PolarizedNeutronProbe([pp, None, None, pp], Aguide=270)
+            exp = Experiment(probe=probe, sample=sample, dz=2)
+            #z,rho,irho,rhoM,thetaM = np.array(exp.magnetic_profile())
+            #z,rho,irho = np.array(exp.step_profile())
+            M = np.array(exp.magnetic_profile())
+            #N = np.array(exp.step_profile())
+            sld_profiles.append([name, M])
+
+        return sld_profiles
+
+    def load_bumps(self):
+        """
+            Use bumps to load MC
+            
+            Example: to histograme first parameter: hist(draw.points[:, 0])
+        
+            We can use draw() instead of sample() [deprecated]
+        """
+        acc = {}
+        for name, m in self.model_list.items():
+            acc[name] = Accumulator(name)
+
+        t0 = time.time()
+        state = dream.state.load_state(self.file_path)
+        state.mark_outliers()
+        
+        drawn = state.draw()
+
+        # If we have a population, only pick 1000 of them
+        if len(drawn.points)>1000:
+            print("Too many points: pruning down to 1000")
+            portion = 1000.0 / len(drawn.points)
+            drawn = state.draw(portion=portion)
+        
+        if not len(drawn.points[0]) == len(self.fit_params):
+            raise RuntimeError("Length of point array is wrong")
+        print("MC file read: %s sec" % (time.time()-t0))
+        for pars in drawn.points:
+            self.replace(pars)
+            profiles = self.convert_to_model()
+            for p in profiles:
+                z, r, _, rM, _ = p[1]
+                acc[p[0]].add(z, r, rM)
+
+        print("Done %s sec" % (time.time()-t0))
+        return acc
             
     def __repr__(self):
         """ Pretty print the fit problem """
@@ -255,14 +364,100 @@ class ReflectivityProblem(object):
 
         return printout
 
-if __name__ == "__main__":
-    filepath = '../test/data/model152both'
+class Accumulator(object):
+    def __init__(self, name='', z_min=-150, z_max=450, z_step=5.0):
+        self.z = np.arange(z_min, z_max, z_step)
+        self.summed = np.zeros(len(self.z)-1)
+        self.sq_summed = np.zeros(len(self.z)-1)
+        self.m_summed = np.zeros(len(self.z)-1)
+        self.m_sq_summed = np.zeros(len(self.z)-1)
+        self.counts = np.zeros(len(self.z)-1)
+        self.z_step = z_step
+        self.name = name
+        
+    def add(self, z, rho, rhoM):
+        """ Add a model to the average """
+        z_ = np.asarray([z[i] for i in range(0, len(z), 2)])
+        rho_ = np.asarray([rho[i+1] for i in range(0, len(rho)-2, 2)])
+        r_out = rebin(z_, rho_, self.z)
+        rhoM_ = np.asarray([rhoM[i+1] for i in range(0, len(rhoM)-2, 2)])
+        rM_out = rebin(z_, rhoM_, self.z)
+        
+        # Compute the average step size so we can normalize after rebinning
+        average_step = np.asarray([z[i+1]-z[i] for i in range(0, len(z)-2, 2) if z[i]>0]).mean()
+        r_out = r_out * average_step / self.z_step
+        rM_out = rM_out * average_step / self.z_step
+
+        self.summed += r_out
+        self.sq_summed += r_out * r_out
+        self.m_summed += rM_out
+        self.m_sq_summed += rM_out * rM_out
+        _counts = [ 1*(z>z_[0] and z<z_[-1]) for z in self.z[:-1] ]
+        self.counts += _counts
+
+    def mean(self):
+        avg = self.summed / self.counts
+        sq_avg = self.sq_summed / self.counts
+        sig = np.sqrt(sq_avg - avg*avg)
+        
+        return avg, sig
     
+    def mean_magnetism(self):
+        avg = self.m_summed / self.counts
+        sq_avg = self.m_sq_summed / self.counts
+        sig = np.sqrt(sq_avg - avg*avg)
+        
+        return avg, sig
+    
+def process(filepath, output):
+    """
+        Process a model output
+    """
+  
     model = ReflectivityProblem(filepath)
     print(model)
     
-    model.replace(range(24))
-    print(model)
+    print("Number of fit pars: %s" % len(model.fit_params))
+    
+    statistics = model.load_bumps()
+    for s in statistics.keys():
+        avg, sig = statistics[s].mean()
+        avg_m, sig_m = statistics[s].mean_magnetism()
+        
+        base_name, ext = os.path.splitext(output)
+        base_name += '_%s' % statistics[s].name
+        _output = base_name+ext
+        with open(_output, 'w') as fd:
+            for i in range(len(avg)):
+                fd.write("%s %s %s %s %s\n" % (statistics[s].z[i], avg[i], sig[i], avg_m[i], sig_m[i]))
+
+                
+if __name__ == "__main__":
+    """
+        Interactive run command
+        
+        python refl1d_model.py -o model152both_stats.txt -m /SNS/REF_M/IPTS-19586/shared/fiting/MGN152Both_3/model152both
+
+        python refl1d_model.py -o test_stats.txt -m /SNS/users/m2d/git/refl1d_analysis/playground/matfit/model152both
+    """
+    # Start/restart options
+    parser = argparse.ArgumentParser(add_help=False)
+
+    # Name of the output file
+    parser.add_argument('-o', metavar='output_name',
+                        help='name of the output file',
+                        dest='output_name', required=True)
+
+    # Location of the model to processe
+    parser.add_argument('-m', metavar='hours',
+                        help='location of the model',
+                        dest='model_path', required=True)
+
+    namespace = parser.parse_args()
+
+    process(namespace.model_path, namespace.output_name)
+
+        
 
     
     
